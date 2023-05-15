439af8eb9ae73f0daf7fdd4f12aaabc7b3988971
Arrowin24 <igor_rudenko.ru@mail.ru>
2023-05-15 11:31:35 +0700

    Initial commit

---
+++ src/main/java/ru/arrowin/test_task/DeviceStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.arrowin.test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/arrowin/test_task/DeviceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arrowin.test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
